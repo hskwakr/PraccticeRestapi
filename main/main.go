59e6700ba9c9fe83a4d6c0aee51ccb02e1b323cc
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-02-10 19:35:09 +0900

    Update main.go

--- main/main.go
+++ main/main.go
@@ -6,13 +6,16 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func main() {
-	app := echo.New()
-
+func setupRoutes(app *echo.Echo) {
 	app.GET("/books/", book.GetBooks)
 	app.GET("/book/:id", book.GetBook)
 	app.POST("/book/", book.NewBook)
 	app.DELETE("/book/:id", book.DeleteBook)
+}
 
+func main() {
+	app := echo.New()
+
+	setupRoutes(app)
 	app.Logger.Fatal(app.Start(":8080"))
 }
