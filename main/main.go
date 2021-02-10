61d49545d73d3eec69a75d83a4bd06dabce401e3
hskwakr <33633391+hskwakr@users.noreply.github.com>
2021-02-10 23:35:43 +0900

    Update main.go

--- main/main.go
+++ main/main.go
@@ -4,6 +4,8 @@ import (
 	"hskwakr/book/book"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func setupRoutes(app *echo.Echo) {
@@ -11,6 +13,16 @@ func setupRoutes(app *echo.Echo) {
 	app.GET("/book/:id", book.GetBook)
 	app.POST("/book/", book.NewBook)
 	app.DELETE("/book/:id", book.DeleteBook)
+}
+
+func initDatabase() {
+	db, err := gorm.Open(mysql.Open("book.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&book.Book{})
 }
 
 func main() {
